18b58b9f0476ebe8ccc4a886e8b108a494296c9a
Kanayo <anayoifediora@live.com>
2023-08-14 21:28:59 +0930

    created the database models

---
+++ server/models/Article.js
@@ -0,0 +1,58 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateformat');
+
+// Create the Article schema
+const articleSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    image: {
+        type: String,
+        required: false,
+        default: 'https://via.placeholder.com/300x200',
+        match:[/\.(png|svg|jpg|jpeg|gif)$/]
+    },
+    articleText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 2800,
+        trim: true,
+    },
+    articleAuthor: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (date) => dateFormat(date),
+    },
+    comments: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Comment',
+        },
+    ],
+},
+{
+    toJSON: {
+        virtuals: true,
+    },
+    id: false,
+});
+
+// Create a virtual called commentCount that retrieves the length of the article's comments array field on query.
+articleSchema.virtual('commentCount')
+.get(function () {
+    return this.comments.length;
+});
+
+// Create the Article model using the ArticleSchema
+const Article = model('Article', articleSchema);
+
+// Export the Article model
+module.exports = Article;
---
+++ server/models/User.js
@@ -0,0 +1,70 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+// Create the User schema
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    email: {
+        type:String,
+        required: true,
+        unique: true,
+        match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/],
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+    },
+    articles: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Article',
+        },
+    ],
+    followers: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        },
+    ],
+
+},
+{
+    toJSON: {
+        virtuals: true,
+    },
+    id: false,
+}
+
+);
+
+// Create a virtual called followerCount that retrieves the length of the user's followers array field on query.
+userSchema.virtual('followerCount')
+.get(function () {
+    return this.followers.length;
+});
+
+// Set up pre-save middleware to create password
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+    next();
+});
+
+// Compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+// Create the User model using the UserSchema
+const User = model('User', userSchema);
+
+// Export the User model
+module.exports = User;
